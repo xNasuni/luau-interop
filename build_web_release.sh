d3ddaf7a38b15fe77d4809418bd5733bce9c73ef
Mia <mia@github.com>
2025-09-12 16:47:34 -0400

    update: add build folders to .gitignore

--- build_web_release.sh
+++ build_web_release.sh
@@ -1,3 +1,3 @@
-emcmake cmake . -DLUAU_BUILD_WEB=ON -DCMAKE_BUILD_TYPE=Release \
+emcmake cmake -B build_web -DLUAU_BUILD_WEB=ON -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_EXE_LINKER_FLAGS="-sSTACK_SIZE=1048576"
-make -j2 Luau.Web
+cmake --build build_web -j2 --target Luau.Web
