4a9a4c2721e79b8140f44eb0b6017feef5a61a15
Mia <mia@github.com>
2025-10-14 20:29:46 -0400

    feature: target web correctly for web support

--- build_web.sh
+++ build_web.sh
@@ -1,3 +1,3 @@
 emcmake cmake -B build_web -DLUAU_BUILD_WEB=ON -DCMAKE_BUILD_TYPE=Release \
-  -DCMAKE_EXE_LINKER_FLAGS="-sSTACK_SIZE=1048576"
+  -DCMAKE_EXE_LINKER_FLAGS="-sSTACK_SIZE=1048576 -sENVIRONMENT=web,node"
 cmake --build build_web -j2 --target Luau.Web
